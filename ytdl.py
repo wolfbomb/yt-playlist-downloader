eee7291a9860ba9510f8f9a9198524bc4beb35c8
nicolasraube <raubenicolas@gmail.com>
2017-08-11 16:02:23 +0200

    checking terminal arg count

--- ytdl.py
+++ ytdl.py
@@ -1,1 +1,11 @@
 import terminal
+
+def main():
+    if terminal.get_argument_count() < 3:
+
+        exit()
+    else:
+        firstArg = terminal.get_argument(1)
+        secondArg = terminal.get_argument(2)
+
+main()
