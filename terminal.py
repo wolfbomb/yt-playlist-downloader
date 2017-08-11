11e2c2bc5c988a89f8ec7eba00b0a1d5cfaf7261
nicolasraube <raubenicolas@gmail.com>
2017-08-11 16:04:26 +0200

    printing help

--- terminal.py
+++ terminal.py
@@ -7,3 +7,27 @@ def get_argument_count():
 
 def get_argument(index):
     return sys.argv[index]
+
+
+def print_green(str):
+    print(colorama.Fore.GREEN + str)
+
+def print_red(str):
+    print(colorama.Fore.RED + str)
+
+def print_help():
+    print("usage:")
+    print("")
+
+    print_green("ytdl.py <url> <dir>")
+    print("<url> - url to the playlist that you want to download")
+    print("<dir> - the directory where the videos should be downloaded to")
+
+    print("")
+    print_green("ytdl.py edit <mp3file>")
+    print("<mp3file> - path to the mp3 file that you want to edit the tags of")
+    print("ytdl will output the id3 tags of the mp3 file")
+    print("you can edit those tags by first using this edit command and then typing the tag that you want to change with an equality sign and the value")
+    print("e.g.:")
+    print("url=https://www.youtube.com/...")
+    print("type \"exit\" to exit editing mode")
--- ytdl.py
+++ ytdl.py
@@ -2,7 +2,7 @@ import terminal
 
 def main():
     if terminal.get_argument_count() < 3:
-        
+        terminal.print_help()
         exit()
     else:
         firstArg = terminal.get_argument(1)
