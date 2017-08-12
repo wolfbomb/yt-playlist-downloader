27cd761878cd5e3d9be34d515a076fdace03d975
nicolasraube <raubenicolas@gmail.com>
2017-08-12 12:04:57 +0200

    remove unnecessary url parameters from video url

--- video.py
+++ video.py
@@ -1,4 +1,8 @@
 class Video:
     def __init__(self, title, url):
         self.title = title
-        self.url = "https://www.youtube.com" + url
+        self.url = "https://www.youtube.com" + self.remove_url_parameters(url)
+
+    def remove_url_parameters(self, url):
+        head, separator, tail = url.partition("&")
+        return head
