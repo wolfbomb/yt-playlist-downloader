77e2eababe783181e2f23799c00a3f9b4e0bedb3
nicolasraube <raubenicolas@gmail.com>
2017-08-16 10:46:23 +0200

    prepared distutils

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = "YT-Playlist-Downloader",
+	version = "1.0",
+	description = "A command-line program for downloading YT-playlists.",
+	author = "Nicolas Raube",
+	packages = ["modules"],
+	scripts = ["ytdl"]
+)
